cbaf7450b73393db788769c1da76638c97faf8d2
minhwankwak <rhkralsghks777@gmail.com>
2025-02-11 00:58:44 +0900

    [탭으로 변경]

--- Assets/Script/UI/Popup/PopupUpgrade.cs
+++ Assets/Script/UI/Popup/PopupUpgrade.cs
@@ -9,6 +9,7 @@ using UniRx;
 [UIPath("UI/Popup/PopupUpgrade")]
 public class PopupUpgrade : UIBase
 {
+
     public enum TabType
     {
         ProductTab,
@@ -37,17 +38,27 @@ public class PopupUpgrade : UIBase
     private TabType CurrentTab = TabType.ProductTab;
 
     [SerializeField]
-    private Button FacilityBtn;
+    private List<Toggle> TabToggles = new List<Toggle>();
 
-    [SerializeField]
-    private Button ProductBtn;
+    private TabType defualtOption = TabType.ProductTab;
 
     protected override void Awake()
     {
         base.Awake();
-        FacilityBtn.onClick.AddListener(OnClickFacility);
-        ProductBtn.onClick.AddListener(OnClickProduct);
         TopCurrencySync();
+
+
+        int iter = 0;
+        foreach (var toggle in TabToggles)
+        {
+            var tabIdx = iter;
+            toggle.isOn = false;
+            toggle.onValueChanged.AddListener(on =>
+            {
+                ChangeTab((TabType)tabIdx, on);
+            });
+            ++iter;
+        }
     }
 
     public void OnClickFacility()
@@ -148,6 +159,117 @@ public class PopupUpgrade : UIBase
         CurrencyTop.MoneyText.text = ProjectUtility.CalculateMoneyToString(GameRoot.Instance.UserData.CurMode.Money.Value);
 
     }
+
+        public void ChangeTab(TabType tab, bool on)
+    {
+        if (CurrentTab == tab) return;
+
+        CurrentTab = tab;
+
+        if (on)
+        {
+            SelectTab(tab);
+        }
+
+
+        foreach(var toggle in TabToggles)
+        {
+            var toggleani = toggle.gameObject.GetComponent<Animator>();
+
+            toggleani.SetTrigger("Normal");
+        }
+
+        var ani = TabToggles[(int)tab].gameObject.GetComponent<Animator>();
+        if (ani != null)
+        {
+            if (on)
+            {
+                // if(IsInit)
+                //     SoundPlayer.Instance.PlaySound("btn");
+
+                if (!TabToggles[(int)tab].isOn)
+                    TabToggles[(int)tab].isOn = true;
+                ani.SetTrigger("Selected");
+            }
+            else
+                ani.SetTrigger("Normal");
+        }
+    }
+
+
+
+    public void SelectTab(TabType tab)
+    {
+
+        switch(tab)
+        {
+            case TabType.FacilityTab:
+            OnClickFacility();
+            break;
+            case TabType.ProductTab:
+            OnClickProduct();
+            break;
+        }
+
+    }
+
+    public override void OnShowBefore()
+    {
+        base.OnShowBefore();
+
+        StartCoroutine(WaitOneFrame());
+    }
+
+
+
+    IEnumerator WaitOneFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+
+        var viewTab = defualtOption;
+        if (!TabToggles[(int)defualtOption].isOn)
+        {
+            TabToggles[(int)defualtOption].isOn = true;
+        }
+        else
+        {
+            var ani = TabToggles[(int)defualtOption].gameObject.GetComponent<Animator>();
+            if (ani != null)
+            {
+                ani.SetTrigger("Selected");
+            }
+            ChangeTab(defualtOption, true);
+        }
+    }
+
+
+
+    IEnumerator OnShowWaitOneFrame()
+    {
+        yield return new WaitForEndOfFrame();
+      
+
+            var ani = TabToggles[(int)defualtOption].gameObject.GetComponent<Animator>();
+        if (ani != null)
+        {
+            ani.SetTrigger("Selected");
+        }
+
+
+    }
+
+
+    public override void OnShowAfter()
+    {
+        base.OnShowAfter();
+
+        StartCoroutine(OnShowWaitOneFrame());
+    }
+
+
+
 
 
     public override void Hide()
